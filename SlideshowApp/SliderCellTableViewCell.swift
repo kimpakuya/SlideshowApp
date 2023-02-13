a120175ba11b8ba9f9dd3ea376802f97f2fad64c
tatsuya obata <at_ssdk@yahoo.co.jp>
2023-02-13 22:11:59 +0900

    2/13

---
+++ SlideshowApp/SliderCellTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  SliderCellTableViewCell.swift
+//  SlideshowApp
+//
+//  Created by 小幡達也 on 2023/02/02.
+//
+
+import UIKit
+
+class SliderCellTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
